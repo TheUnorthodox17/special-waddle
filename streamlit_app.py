532681f65a725cb1f6f94eff87b6c1ef06d0c084
TheUnorthodox17 <127400969+TheUnorthodox17@users.noreply.github.com>
2023-03-09 22:44:05 +0530

    Update streamlit_app.py
    corrected the typo error w.r.t Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Mom''s New Healthy Diner')
  
-streamlit.header('🥣 Breakfast Favorates')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(' Breakfast Favorates')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
